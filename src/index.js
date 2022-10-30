86c3b89b569526b1b715cf148002632593a0b473
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-10-30 18:18:08 +0100

    commit bugfix
    bugfix after changes where I create new file components.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const loadedImagesInfo = document.querySelector(".loaded-images");
 let page = 1;
 let amountImages = "";
 let maxAmountImages = "";
-let scrollSetupValue = 65;
+// let scrollSetupValue = 65;
 let pageNumber = 1;
 let sumLoadedImages = IMAGES_PER_PAGE;
 let sumOfPages = "";
@@ -48,6 +48,8 @@ const fetchApi = async (inputValue,nextPage) => {
     pageNumber = 1;
     sumOfPages = 0;
     sumLoadedImages = IMAGES_PER_PAGE;
+    pageNumberInfo.innerHTML = "";
+    loadedImagesInfo.innerHTML = "";
     return
   }
   try {
@@ -119,35 +121,33 @@ function showSimplelightbox(){
   gallery.refresh();
 };
 
-function scrollDelay(){
-  window.scrollTo({top: scrollSetupValue, behavior: "smooth"});
-  scrollSetupValue-=5;
-};
+// function scrollDelay(){
+//   window.scrollTo({top: scrollSetupValue, behavior: "smooth"});
+//   scrollSetupValue-=5;
+// };
 
-const scrollUp = () => {
-  const nav = 70;
-  window.scrollTo({top: nav, behavior: "smooth"});
-  setTimeout(() => {
-    const intervalId = setInterval(() => {
-      scrollDelay();
-        if (scrollSetupValue<=-400) {
-        clearInterval(intervalId)
-      }
-    }, 10);   
-  }, 700);
-}
+// const scrollUp = () => {
+//   const nav = 0;
+//   window.scrollTo({top: nav, behavior: "smooth"});
+//   // setTimeout(() => {
+//   //   const intervalId = setInterval(() => {
+//   //     scrollDelay();
+//   //       if (scrollSetupValue<=-400) {
+//   //       clearInterval(intervalId)
+//   //     }
+//   //   }, 10);   
+//   // }, 700);
+// }
 
 function loadMoreResults() {
   page+=1;
   fetchApi(searchForm.searchQuery.value, page);
   amountImages = IMAGES_PER_PAGE * page;
-  setTimeout(() => {
-    scrollUp()
-  }, 600);
+  // setTimeout(() => {
+  //   scrollUp()
+  // }, 600);
 
-  pageNumber+=1;
-  sumLoadedImages+=IMAGES_PER_PAGE;
-  console.log(pageNumber,sumLoadedImages, sumOfPages);
+  amountLoadedImages()
 
   pageNumberInfo.innerHTML = `Page ${pageNumber} &#47; ${sumOfPages}`;
   loadedImagesInfo.innerHTML = `Loaded images ${sumLoadedImages} &#47; ${maxAmountImages}`;
@@ -158,85 +158,3 @@ function amountLoadedImages(){
   sumLoadedImages+=IMAGES_PER_PAGE;
   console.log(pageNumber,sumLoadedImages, sumOfPages);
 }
-
-// ==================================KOPY==========================================
-
-// const galleryContainer = document.querySelector(".gallery");
-// const searchForm = document.getElementById("search-form");
-// const loadMoreBtn = document.querySelector(".load-more");
-
-// let page = 1;
-// let amountImages = "";
-// let maxAmountImages = "";
-
-// searchForm.addEventListener("click", (evt)=>{
-//   evt.preventDefault()
-//   fetchApi(searchForm.searchQuery.value, page)
-// });
-
-// loadMoreBtn.addEventListener("click", loadMoreResults);
-
-// const fetchApi = async (inputValue,nextPage) => {
-//   if (!inputValue) {
-//     galleryContainer.innerHTML = "";
-//     loadMoreBtn.style.display="none";
-//     return
-//   }
-//   try {
-//     const searchText = inputValue.split(" ").join('+');
-//     const params = new URLSearchParams({
-//     key: API_KEY,
-//     q: searchText,
-//     image_type: "photo",
-//     orientation: "horizontal",
-//     safesearch: true,
-//     page: nextPage,
-//     per_page: IMAGES_PER_PAGE
-//   })
-  
-//     const response = await axios(FETCH_API_URL + "?" + params);
-//     maxAmountImages = response.data.totalHits;
-//     if (response.data.hits.length==0) {
-//       Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-//       galleryContainer.innerHTML = "";
-//       loadMoreBtn.style.display="none";
-//       return
-//     };
-//     if (amountImages>=maxAmountImages) {
-//       Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-//       loadMoreBtn.style.display="none";
-//       return
-//     }
-//     galleryContainer.innerHTML = response.data.hits.map(image => {
-//       console.log(image);
-//       loadMoreBtn.style.display="block";
-//       return `
-//         <div class="photo-card">
-//           <img src="${image.previewURL}" alt=" " loading="lazy" data-large="${image.largeImageURL}"/>
-//           <div class="info">
-//             <p class="info-item">
-//               <b>Likes</b>${image.likes}
-//             </p>
-//             <p class="info-item">
-//               <b>Views</b>${image.views}
-//             </p>
-//             <p class="info-item">
-//               <b>Comments</b>${image.comments}
-//             </p>
-//             <p class="info-item">
-//               <b>Downloads</b>${image.downloads}
-//             </p>
-//           </div>
-//         </div>
-//         `
-//     })
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// function loadMoreResults() {
-//   page+=1;
-//   fetchApi(searchForm.searchQuery.value, page);
-//   amountImages = IMAGES_PER_PAGE * page;
-// };
