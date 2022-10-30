7648040929b0f45b8d66daa04c8f85468434f646
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-10-30 13:07:47 +0100

    completed funcioning task
    next step is create components file and doing exports imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 FETCH_API_URL = "https://pixabay.com/api/";
 API_KEY = "26135070-3e729b9e8c0999352fd85e768";
@@ -10,11 +10,17 @@ IMAGES_PER_PAGE = 40;
 const galleryContainer = document.querySelector(".gallery");
 const searchForm = document.getElementById("search-form");
 const loadMoreBtn = document.querySelector(".load-more");
+const pageNumberInfo = document.querySelector(".page-number");
+const loadedImagesInfo = document.querySelector(".loaded-images");
 
 let page = 1;
 let amountImages = "";
 let maxAmountImages = "";
 let scrollSetupValue = 65;
+let pageNumber = 1;
+let sumLoadedImages = IMAGES_PER_PAGE;
+let sumOfPages = "";
+
 
 searchForm.addEventListener("click", (evt)=>{
   evt.preventDefault();
@@ -23,17 +29,15 @@ searchForm.addEventListener("click", (evt)=>{
     if (evt.target.tagName==="BUTTON" && searchForm.searchQuery.value.length>0) {
       setTimeout(() => {
         Notiflix.Notify.info(`Hooray! We found ${maxAmountImages} images.`);
+        sumOfPages = Math.ceil(maxAmountImages / sumLoadedImages);
+        pageNumberInfo.innerHTML = `Page ${pageNumber} &#47; ${sumOfPages}`;
+        loadedImagesInfo.innerHTML = `Loaded images ${sumLoadedImages} &#47; ${maxAmountImages}`;
+        // amountLoadedImages();
+        console.log(maxAmountImages ,sumOfPages);
     }, 500);
   }
-
-  setTimeout(() => {
-    const gallery = new SimpleLightbox(".photo-card a", {
-      captionDelay: 250,
-      captionsData: "alt",
-    });
-    gallery.on("show.simplelightbox");
-  }, 200);
 });
+
 
 loadMoreBtn.addEventListener("click", loadMoreResults);
 
@@ -41,6 +45,9 @@ const fetchApi = async (inputValue,nextPage) => {
   if (!inputValue) {
     galleryContainer.innerHTML = "";
     loadMoreBtn.style.display="none";
+    pageNumber = 1;
+    sumOfPages = 0;
+    sumLoadedImages = IMAGES_PER_PAGE;
     return
   }
   try {
@@ -69,6 +76,7 @@ const fetchApi = async (inputValue,nextPage) => {
     if (amountImages>=maxAmountImages) {
       Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
       loadMoreBtn.style.display="none";
+      sumLoadedImages = maxAmountImages;
       return
     }
     galleryContainer.innerHTML = response.data.hits.map(image => {
@@ -76,7 +84,7 @@ const fetchApi = async (inputValue,nextPage) => {
       return `
         <div class="photo-card">
           <a href="${image.largeImageURL}">
-            <img src="${image.previewURL}" alt=" " loading="lazy" data-large="${image.largeImageURL}"/>
+            <img src="${image.previewURL}" alt="${image.tags}" loading="lazy" data-large="${image.largeImageURL}"/>
           </a>  
           <div class="info">
             <p class="info-item">
@@ -95,40 +103,29 @@ const fetchApi = async (inputValue,nextPage) => {
         </div>
         `
     });
-    // const gallery = new SimpleLightbox(".photo-card a", {
-    //   captionDelay: 250,
-    //   captionsData: "alt",
-    // });
-    // gallery.on("show.simplelightbox");
+    showSimplelightbox()
   } catch (error) {
     console.log(error);
   }
 };
 
 
-function showSimplelightbox(event){
-  
-  console.log(event.target.tagName);
-  if (event.target.tagName==="A") {
-    event.preventDefault();
-    const gallery = new SimpleLightbox(".photo-card a", {
-      captionDelay: 250,
-      captionsData: "alt",
-    });
-    return gallery.on("show.simplelightbox");
-  };
+function showSimplelightbox(){
+  const gallery = new SimpleLightbox(".photo-card a", {
+    captionDelay: 250,
+    captionsData: "alt",
+  });
+  gallery.on("show.simplelightbox");
+  gallery.refresh();
 };
-
-const nav = 70;
-
 
 function scrollDelay(){
   window.scrollTo({top: scrollSetupValue, behavior: "smooth"});
   scrollSetupValue-=5;
-  console.log(scrollSetupValue);
 };
 
 const scrollUp = () => {
+  const nav = 70;
   window.scrollTo({top: nav, behavior: "smooth"});
   setTimeout(() => {
     const intervalId = setInterval(() => {
@@ -141,14 +138,26 @@ const scrollUp = () => {
 }
 
 function loadMoreResults() {
-  qqq=65;
   page+=1;
   fetchApi(searchForm.searchQuery.value, page);
   amountImages = IMAGES_PER_PAGE * page;
   setTimeout(() => {
     scrollUp()
   }, 600);
+
+  pageNumber+=1;
+  sumLoadedImages+=IMAGES_PER_PAGE;
+  console.log(pageNumber,sumLoadedImages, sumOfPages);
+
+  pageNumberInfo.innerHTML = `Page ${pageNumber} &#47; ${sumOfPages}`;
+  loadedImagesInfo.innerHTML = `Loaded images ${sumLoadedImages} &#47; ${maxAmountImages}`;
 };
+
+function amountLoadedImages(){
+  pageNumber+=1;
+  sumLoadedImages+=IMAGES_PER_PAGE;
+  console.log(pageNumber,sumLoadedImages, sumOfPages);
+}
 
 // ==================================KOPY==========================================
 
